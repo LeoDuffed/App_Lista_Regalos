f157a9fd8eda97d87e020060194eb67a374c67c8
LeoDuffed <carlosmroder@gmail.com>
2024-12-17 16:04:54 -0600

    Empezando la app

--- App_Lista_Regalos.py
+++ App_Lista_Regalos.py
@@ -1,3 +1,6 @@
+#Creado por Leonardo Martinez Peña
+# Iniciado el 16/12/2024
+
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
@@ -12,15 +15,53 @@ from kivy.core.window import Window
 
 storage = JsonStore("lista_regalos.jason")
 
-class PantallaInicio(Screen):
+class Pantalla_Inicio(Screen):
     def __init__(self, **kawargs):
-        regalos().__init__(**kawargs)
+        super().__init__(**kawargs)
 
         layout = BoxLayout(orientation = 'vertical', padding = 20, spacing = 10)
 
+        welcomerLabel = Label (text = "Tus listas de regalos", font_size = '30sp', color = (0,0,0,1))
+        layout.add_widget(welcomerLabel)
+
+        boton_pantalla_personas = Button(text = "Inicia tu lista", pos_hint = {"center_x": 0.5}, background_color = (0.5,1,0,1))
+        boton_pantalla_personas.bind(on_press = self.CambiarPersonas)
+        layout.add_widget(boton_pantalla_personas)
+
+        self.add_widget(layout)
+
+    def CambiarPersonas(self, instance):
+        self.manager.current = 'listaIni'
+
+                                         
+
+class Pantalla_Aniadir_Lista_Personas(Screen):
+    def __init__(self, **k):
+        super().__init__(**k)
+
+        Window.clearcolor = (1, 1, 0.9, 1)
+
+        layout = BoxLayout(orientation = 'vertical', padding = 20, spacing = 10)
+
+        welcomerLabel = Label (text = "Crea tus listas", font_size = '30sp', color = (0,0,0,1))
+        layout.add_widget(welcomerLabel)
+
+        boton_volver = Button(text = "Volver", pos_hint = {"center_x":0.5}, background_color = (1, 0.7, 0.8, 1))
+        boton_volver.bind(on_press = self.CambiarVolver)
+        layout.add_widget(boton_volver)
+
+        self.add_widget(layout)
+
+    def CambiarVolver(self, instance):
+        self.manager.current = "inicio"
+    
+
 class ListaRegalos(App):
     def build(self):
-
+        
+        sm = ScreenManager()
+        sm.add_widget(Pantalla_Inicio(name = 'inicio'))
+        sm.add_widget(Pantalla_Aniadir_Lista_Personas(name = 'listaIni'))
         return sm
 
 
